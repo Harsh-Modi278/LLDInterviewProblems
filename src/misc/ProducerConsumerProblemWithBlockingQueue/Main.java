1f2608537a4247a8f854cfbe3e1d41643738681b
Harsh-Modi278 <harshmodi278@gmail.com>
2025-06-22 23:43:20 +0530

    producer consumer with BlockingQueue

---
+++ src/misc/ProducerConsumerProblemWithBlockingQueue/Main.java
@@ -0,0 +1,13 @@
+package misc.ProducerConsumerProblemWithBlockingQueue;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Buffer buffer = new Buffer(5);
+        Thread producerThread = new Thread(new ProducerTask(buffer));
+        Thread consumerThread = new Thread(new ConsumerTask(buffer));
+
+        producerThread.start();
+        consumerThread.start();
+    }
+}
---
+++ src/misc/ProducerConsumerProblemWithBlockingQueue/ProducerTask.java
@@ -0,0 +1,23 @@
+package misc.ProducerConsumerProblemWithBlockingQueue;
+
+public class ProducerTask implements Runnable {
+    private final Buffer buffer;
+
+    public ProducerTask(Buffer buffer) {
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void run() {
+        int value = 0;
+        try {
+            while (true) {
+                buffer.put(value);
+                value++;
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
