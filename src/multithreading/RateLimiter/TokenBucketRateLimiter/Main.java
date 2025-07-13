a9f1978d98ba007436f40ab2c7583fef7ca32de4
Harsh-Modi278 <harshmodi278@gmail.com>
2025-07-13 18:15:08 +0530

    pros and cons of Token bucket vs Leaky bucket

--- src/multithreading/RateLimiter/TokenBucketRateLimiter/Main.java
+++ src/multithreading/RateLimiter/TokenBucketRateLimiter/Main.java
@@ -24,3 +24,21 @@ public class Main {
 //        t2.start();
     }
 }
+
+/**
+ * - Token Bucket
+ *   - is preferred when flexibility and burst handling are needed.
+ *   - No packet/request loss (if tokens are available): Requests wait until tokens are available rather than being dropped immediately.
+ *
+ *   * How Token Bucket Handles Requests
+ *     - Tokens accumulate in the bucket at a fixed rate, up to a maximum capacity
+ *     - When a request arrives:
+ *       1. If there are enough tokens, the request is allowed and tokens are removed
+ *       2. If there are not enough tokens, the request is rejected or must wait until tokens are refilled,
+ *          depending on the system's implementation
+ *
+ * - Leaky Bucket
+ *  - is ideal when a steady, predictable flow is required and bursts must be strictly controlled.
+ *  - Potential data/request loss: If incoming rate exceeds the leak rate, excess requests are discarded.
+ *
+ */
